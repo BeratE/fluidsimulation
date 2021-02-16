28ef39678b23e66b4c9799808af69c3e1d04e790
Berat Ertural <beertural@gmail.com>
2021-02-16 14:38:01 +0100

    report

--- report/auto/references.el
+++ report/auto/references.el
@@ -3,6 +3,13 @@
  (lambda ()
    (LaTeX-add-bibitems
     "macklin2013"
-    "pineda1988"))
+    "pineda1988"
+    "akinci2013"
+    "akinci2012"
+    "lorensen1987"
+    "monaghan1992"
+    "muller2007"
+    "ihmsen2011"
+    "bender2015"))
  :bibtex)
 
--- report/auto/report.el
+++ report/auto/report.el
@@ -5,10 +5,10 @@
                      '(("article" "11pt" "letterpaper" "twocolumn")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("babel" "english") ("inputenc" "utf8") ("geometry" "margin=1.0in")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -27,10 +27,14 @@
    (LaTeX-add-labels
     "sec:introduction"
     "sec:methods"
-    "fig:systemuml"
+    "eq:weight"
+    "eq:pressure"
+    "eg:pbfconstraint"
+    "eg:deltax"
     "fig:mesh"
     "eq:localtriangle"
     "fig:trianglesampling"
+    "fig:systemuml"
     "sec:results"
     "sec:conclusion"
     "sec:future")
